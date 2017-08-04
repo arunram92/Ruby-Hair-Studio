bda7bb381eb9805bb69d8fea1d956c11c8c4288f
Y.Arun Ramanathan <y.arunram@gmail.com>
2017-08-04 05:39:17 +0530

    Ruby Hair Studio Website

---
+++ ruby/Index.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Ruby Hair Studio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/form-elements.css">
+  <link rel="stylesheet" href="css/custom.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ > 
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage"><img src="img/logo.png"/></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#home">HOME</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#gallery">GALLERY</a></li>
+        <li><a href="#testimonials">TESTIMONIALS</a></li>
+		<li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="home" class="container-fluid">
+  <div class="row">
+    <!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="data1/images/1.jpg" alt="Welcome to Ruby Hair Studio's Website " title="Welcome to Ruby Hair Studio's Website" id="wows1_0"/></li>
+		<li><a href="#"><img src="data1/images/3.jpg" alt="jquery slider" title="What you wear begins with your hair and face." id="wows1_1"/></a></li>
+		<li><img src="data1/images/8.jpg" alt="We are Ottawa's one of the premier salons" title="We are Ottawa's one of the premier salons." id="wows1_2"/></li>
+	</ul></div>
+	<div class="ws_bullets"><div>
+		<a href="#" title="Welcome to Ruby Hair Studio's Website"><span><img src="data1/tooltips/1.jpg" alt="Welcome to Ruby Hair Studio's Website"/>1</span></a>
+		<a href="#" title="What you wear begins with your hair and face."><span><img src="data1/tooltips/3.jpg" alt="What you wear begins with your hair and face."/>2</span></a>
+		<a href="#" title="We are Ottawa's one of the premier salons"><span><img src="data1/tooltips/8.jpg" alt="We are Ottawa's one of the premier salons"/>3</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"></div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+  </div>
+
+
+<!-- Container (About Section) -->
+<div id="about" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-6">
+<br/>  
+
+    <!--  <h2> Welcome to Ruby Hair studio </h2>
+	what you wear begins with your hair and face  -->
+						<img src="img/about.png" class="imageicon">
+						<h3>About</h3>
+						<br/>
+						<p>
+							Welcome to Ruby Hair Studio's Website. What you wear begins with your hair and face. We are Ottawa's premier hair salon. This is sample content. 
+							Welcome to Ruby Hair Studio's Website. What you wear begins with your hair and face. We are Ottawa's premier hair salon. This is sample content.
+							Welcome to Ruby Hair Studio's Website. What you wear begins with your hair and face. We are Ottawa's premier hair salon. This is sample content.
+							Welcome to Ruby Hair Studio's Website. What you wear begins with your hair and face. We are Ottawa's premier hair salon. This is sample content.
+						</p>
+						<br/>
+						<a href="#" class="readmore"> Make An Appointment </a>
+	
+						<div class="clearfix"></div>
+	
+    
+    </div>
+    <div class="col-sm-6">
+<br/>  
+
+	<img src="img/contacticon.ico" class="imageicon">
+						<h3>Contact</h3>
+						<br/>
+						<p class="contactdetails">
+						<b>
+						<img class="timeicon" src="img/email.png" height="30" width="30"/>
+						<span class="time_content"><a class="time_content" href="mailto:m.fayad@sjsu.edu"> sample@ruby.com </a></span>
+						<br/>
+						<br/> 
+						<img class="timeicon" src="img/mobile.png" height="30" width="30"/>
+						<span class="time_content"><a href="tel:4089247364">  (123) 4567890 </a></span>
+						<br/>
+						<br/>
+						<img class="timeicon" src="img/venue.png" height="30" width="30"/>
+						<span class="venue_content"> Ottawa </span>
+						</b>
+						</p>
+						
+  <!--    <img class="img-responsive" src="img/stark.png"/> -->
+    </div>
+  </div>
+</div>
+</div>
+<br>
+</div>
+<!-- Container (Services Section) -->
+<div id="services" class="container-fluid text-center">
+  <h2>Services</h2>
+    <div class="col-sm-3 service service1">
+	  <br/>
+      <img src="img/haircutting.png"  height="100" width="100"/>
+	  <h3> Hair Cutting </h3>
+	  <br/>
+	  <p> Lorem ipsum dolor sit amet, te duo labitur dolores. </p>
+	      </div>
+	
+	<div class="col-sm-3 service service2">
+	  <br/>
+      <img src="img/shaving.png"  height="100" width="100"/>
+	  <h3> Shaving </h3>
+	  <br/>
+	  <p> Lorem ipsum dolor sit amet, te duo labitur dolores. </p>
+    </div>
+	
+	<div class="col-sm-3 service service3">
+	  <br/>
+      <img src="img/shave.png"  height="100" width="100"/>
+	  <h3> Trimming </h3>
+	  <br/>
+	  <p> Lorem ipsum dolor sit amet, te duo labitur dolores. </p>
+    </div>
+	
+	<div class="col-sm-3 service service4">
+	  <br/>
+      <img src="img/style.png"  height="100" width="100"/>
+	  <h3> Styling </h3>
+	  <br/>
+	  <p> Lorem ipsum dolor sit amet, te duo labitur dolores. </p>
+    </div>
+   <a href="#" class="readmore"> Make An Appointment </a>
+   <br/>
+</div>
+
+
+<!-- Container (Gallery Section) -->
+<div id="gallery" class="container-fluid text-center">
+  <h2>GALLERY</h2>
+  COMING SOON...
+  
+</div>
+
+<!-- Container (testimonials Section) -->
+<div id="testimonials" class="container-fluid text-center bg-grey">
+  
+  <h2>Testimonials</h2>
+  <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <h4>"This company is the best. I am so happy with the result!"<br><span>Michael Roe, Vice President, Comment Box</span></h4>
+      </div>
+      <div class="item">
+        <h4>"One word... WOW!!"<br><span>John Doe, Salesman, Rep Inc</span></h4>
+      </div>
+      <div class="item">
+        <h4>"Could I... BE any more happy with this company?"<br><span>Chandler Bing, Actor, FriendsAlot</span></h4>
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+
+<div id="contact" class="container-fluid text-center">
+  <h2 class="text-center">CONTACT</h2>
+   <div class="row">
+    <div class="col-sm-12 slideanim">
+		<div class="row">
+			 <div class="col-sm-2 form-group">
+			 </div>
+			<div class="col-sm-4 form-group">
+			  <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+			</div>
+			<div class="col-sm-4 form-group">
+			  <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+			</div>
+			<div class="col-sm-2 form-group">
+			 </div>
+       </div>
+	   <div class="row">
+			 <div class="col-sm-2 form-group">
+			 </div>
+			<div class="col-sm-4 form-group">
+			  <input class="form-control" id="name" name="name" placeholder="Mobile Number" type="text" required>
+			</div>
+			<div class="col-sm-4 form-group">
+			  <input class="form-control" id="email" name="email" placeholder="Topic" type="email" required>
+			</div>
+			<div class="col-sm-2 form-group">
+			 </div>
+       </div>
+	   <div class="row">
+			 <div class="col-sm-2 form-group">
+			 </div>
+			 <div class="col-sm-8 form-group">
+			  <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+			</div>
+			<div class="col-sm-2 form-group">
+			 </div>
+       </div>
+	   <div class="row">
+			<button class="readmore" type="submit">Send an inquiry</button>
+			
+       </div>
+	  
+	</div>
+ </div>
+</div>
+
+
+
+</div>
+
+
+
+
+<footer class="container-fluid text-center">
+   <div class="text-center center-block">
+              <a href="#"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+              <a href="#"><i id="social-em" class="fa fa-github-square fa-3x social"></i></a>
+              <a href="#"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+              <a href="#"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+</div>
+</footer>
+
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+  });
+})
+</script>
+
+</body>
+</html>
